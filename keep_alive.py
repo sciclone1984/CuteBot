fea16655acb0c78e1e57d9cf0efc3790433e3e33
Alex Sitterer <39037112+sciclone1984@users.noreply.github.com>
2022-08-11 18:12:50 +0000

    Updated keep_alive website, included bot link.

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "If you're reading this, CuteBot is active."
+    return "If you're reading this, CuteBot is active.\n\nIf you'd like to add CuteBot to your server, you can use the following link:\nhttps://discord.com/api/oauth2/authorize?client_id=969093006512754728&permissions=277025410048&scope=bot%20applications.commands"
 
 def run():
   app.run(host='0.0.0.0',port=8080)
